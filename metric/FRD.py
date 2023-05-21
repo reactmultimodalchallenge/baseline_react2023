32ee02b2a86e30defc7537278f14eabe40a53ba9
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2023-05-21 12:21:49 +0800

    Update FRD.py

--- metric/FRD.py
+++ metric/FRD.py
@@ -4,23 +4,50 @@ from tslearn.metrics import dtw
 from functools import partial
 import multiprocessing as mp
 
+def compute_FRD(args, pred, em, val_test='val'):
+    if val_test == 'val':
+        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_val.npy'))
+    else:
+        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_test.npy'))
+    all_FRD_list = []
+    for i in range(pred.shape[1]):
+        FRD_list = []
+        for k in range(pred.shape[0]):
+            neighbour_index = np.argwhere(neighbour_matrix[k] == 1).reshape(-1)
+            neighbour_index_len = len(neighbour_index)
+            dwt_list = []
+            for n_index in range(neighbour_index_len):
+                emotion = em[neighbour_index[n_index]]
+                res = 0
+                for st, ed, weight in [(0, 15, 1 / 15), (15, 17, 1), (17, 25, 1 / 8)]:
+                    res += weight * dtw(pred[k, i].numpy().astype(np.float32)[:, st: ed],
+                                        emotion.numpy().astype(np.float32)[:, st: ed])
+                dwt_list.append(res)
+            min_dwt = min(dwt_list)
+            FRD_list.append(min_dwt)
+        all_FRD_list.append(np.mean(FRD_list))
+    return sum(all_FRD_list)
+
 def _func(k_neighbour_matrix, k_pred, em=None):
     neighbour_index = np.argwhere(k_neighbour_matrix == 1).reshape(-1)
     neighbour_index_len = len(neighbour_index)
-    dwt_list = []
-    for n_index in range(neighbour_index_len):
-        emotion = em[neighbour_index[n_index]]
-        res = 0
-        for st, ed, weight in [(0, 15, 1 / 15), (15, 17, 1), (17, 25, 1 / 8)]:
-            res += weight * dtw(k_pred.numpy().astype(np.float32)[:, st: ed], emotion.numpy().astype(np.float32)[:, st: ed])
-        dwt_list.append(res)
-    min_dwt = min(dwt_list)
-    return min_dwt
+    min_dwt_sum = 0
+    for i in range(k_pred.shape[0]):
+        dwt_list = []
+        for n_index in range(neighbour_index_len):
+            emotion = em[neighbour_index[n_index]]
+            res = 0
+            for st, ed, weight in [(0, 15, 1 / 15), (15, 17, 1), (17, 25, 1 / 8)]:
+                res += weight * dtw(k_pred[i].numpy().astype(np.float32)[:, st: ed], emotion.numpy().astype(np.float32)[:, st: ed])
+            dwt_list.append(res)
+        min_dwt_sum += min(dwt_list)
+    return min_dwt_sum
+
 
 def compute_FRD_mp(args, pred, em, val_test='val', p=8):
     # pred: N 10 750 25
     # speaker: N 750 25
-    
+
     if val_test == 'val':
         neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_val.npy'))
     else:
@@ -31,27 +58,5 @@ def compute_FRD_mp(args, pred, em, val_test='val', p=8):
         # use map
         _func_partial = partial(_func, em=em)
         FRD_list += pool.starmap(_func_partial, zip(neighbour_matrix, pred))
-        
-    return np.mean(FRD_list)
-
-
-def compute_FRD(args, pred, em, val_test='val'):
-    FRD_list = []
-    if val_test == 'val':
-        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_val.npy'))
-    else:
-        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_test.npy'))
-    for k in range(pred.shape[0]):
-        neighbour_index = np.argwhere(neighbour_matrix[k] == 1).reshape(-1)
-        neighbour_index_len = len(neighbour_index)
-        dwt_list = []
-        for n_index in range(neighbour_index_len):
-            emotion = em[neighbour_index[n_index]]
-            res = 0
-            for st, ed, weight in [(0, 15, 1 / 15), (15, 17, 1), (17, 25, 1 / 8)]:
-                res += weight * dtw(pred[k].numpy().astype(np.float32)[:, st: ed], emotion.numpy().astype(np.float32)[:, st: ed])
-            dwt_list.append(res)
-        min_dwt = min(dwt_list)
-        FRD_list.append(min_dwt)
 
     return np.mean(FRD_list)
