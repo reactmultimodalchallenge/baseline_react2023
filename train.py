d841008dc0d029d0a7aaa2d5c0432d69e8b89df9
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2023-06-04 19:17:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,12 +7,11 @@ import torch.optim as optim
 import argparse
 from tqdm import tqdm
 import logging
-from dataset import ReactionDataset
 from model import TransformerVAE
 from utils import AverageMeter
 from render import Render
-from model.losses import VAELoss
 from dataset import get_dataloader
+from model.losses import VAELoss, div_loss
 
 def parse_arg():
     parser = argparse.ArgumentParser(description='PyTorch Training')
@@ -30,61 +29,73 @@ def parse_arg():
     parser.add_argument('--optimizer-eps', default=1e-8, type=float)
     parser.add_argument('--img-size', default=256, type=int, help="size of train/test image data")
     parser.add_argument('--crop-size', default=224, type=int, help="crop size of train/test image data")
-    parser.add_argument('-seq-len', default=751, type=int, help="length of clip")
+    parser.add_argument('-max-seq-len', default=751, type=int, help="max length of clip")
+    parser.add_argument('--clip-length', default=256, type=int, help="len of video clip")
     parser.add_argument('--window-size', default=8, type=int, help="prediction window-size for online mode")
     parser.add_argument('--feature-dim', default=128, type=int, help="feature dim of model")
     parser.add_argument('--audio-dim', default=78, type=int, help="feature dim of audio")
     parser.add_argument('--_3dmm-dim', default=58, type=int, help="feature dim of 3dmm")
     parser.add_argument('--emotion-dim', default=25, type=int, help="feature dim of emotion")
     parser.add_argument('--online', action='store_true', help='online / offline method')
+    parser.add_argument('--render', action='store_true', help='w/ or w/o render')
     parser.add_argument('--momentum', type=float, default=0.99)
     parser.add_argument('--outdir', default="./results", type=str, help="result dir")
     parser.add_argument('--device', default='cuda', type=str, help="device: cuda / cpu")
     parser.add_argument('--gpu-ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
     parser.add_argument('--kl-p', default=0.0002, type=float, help="hyperparameter for kl-loss")
+    parser.add_argument('--div-p', default=10, type=float, help="hyperparameter for div-loss")
 
     args = parser.parse_args()
     return args
 
 
 # Train
-def train(model, train_loader, optimizer, criterion):
+def train(args, model, train_loader, optimizer, criterion):
     losses = AverageMeter()
     rec_losses = AverageMeter()
     kld_losses = AverageMeter()
+    div_losses = AverageMeter()
 
     model.train()
-    for batch_idx, (speaker_video_clip, speaker_audio_clip, _, _, _, listener_emotion, listener_3dmm, _) in enumerate(tqdm(train_loader)):
+    for batch_idx, (speaker_video_clip, speaker_audio_clip, _, _, _, _, listener_emotion, listener_3dmm, _) in enumerate(tqdm(train_loader)):
         if torch.cuda.is_available():
-            speaker_video_clip, speaker_audio_clip, listener_emotion, listener_3dmm = \
-                speaker_video_clip[:,:750].cuda(), speaker_audio_clip[:,:750].cuda(), listener_emotion[:,:750].cuda(), listener_3dmm[:,:750].cuda()
+            speaker_video_clip, speaker_audio_clip,  listener_emotion, listener_3dmm = \
+                speaker_video_clip.cuda(), speaker_audio_clip.cuda(),  listener_emotion.cuda(), listener_3dmm.cuda()
 
-        optimizer.zero_grad()
         listener_3dmm_out, listener_emotion_out, distribution = model(speaker_video_clip, speaker_audio_clip)
         loss, rec_loss, kld_loss = criterion(listener_emotion, listener_3dmm, listener_emotion_out, listener_3dmm_out,
                                              distribution)
 
+        with torch.no_grad():
+            listener_3dmm_out_, listener_emotion_out_, _ = model(speaker_video_clip, speaker_audio_clip)
+
+        d_loss = div_loss(listener_3dmm_out_, listener_3dmm_out) + div_loss(listener_emotion_out_, listener_emotion_out)
+
+        loss = loss + args.div_p * d_loss
+
         losses.update(loss.data.item(), speaker_video_clip.size(0))
         rec_losses.update(rec_loss.data.item(), speaker_video_clip.size(0))
         kld_losses.update(kld_loss.data.item(), speaker_video_clip.size(0))
+        div_losses.update(d_loss.data.item(), speaker_video_clip.size(0))
 
         loss.backward()
         optimizer.step()
-    return losses.avg, rec_losses.avg, kld_losses.avg
+    return losses.avg, rec_losses.avg, kld_losses.avg, div_losses.avg
 
 
 
 
-# Train
+# Validation
 def val(args, model, val_loader, criterion, render, epoch):
     losses = AverageMeter()
     rec_losses = AverageMeter()
     kld_losses = AverageMeter()
     model.eval()
-    for batch_idx, (speaker_video_clip, speaker_audio_clip, _, _, _, listener_emotion, listener_3dmm, listener_references) in enumerate(tqdm(val_loader)):
+    model.reset_window_size(8)
+    for batch_idx, (speaker_video_clip, speaker_audio_clip, _, _, _, _, listener_emotion, listener_3dmm, listener_references) in enumerate(tqdm(val_loader)):
         if torch.cuda.is_available():
             speaker_video_clip, speaker_audio_clip, listener_emotion, listener_3dmm, listener_references = \
-                speaker_video_clip[:,:750].cuda(), speaker_audio_clip[:,:750].cuda(), listener_emotion[:,:750].cuda(), listener_3dmm[:,:750].cuda(), listener_references[:,:750].cuda()
+                speaker_video_clip.cuda(), speaker_audio_clip.cuda(), listener_emotion.cuda(), listener_3dmm.cuda(), listener_references.cuda()
 
         with torch.no_grad():
             listener_3dmm_out, listener_emotion_out, distribution = model(speaker_video_clip, speaker_audio_clip)
@@ -95,28 +106,33 @@ def val(args, model, val_loader, criterion, render, epoch):
             rec_losses.update(rec_loss.data.item(), speaker_video_clip.size(0))
             kld_losses.update(kld_loss.data.item(), speaker_video_clip.size(0))
 
-            val_path = os.path.join(args.outdir, 'results_videos', 'val')
-            if not os.path.exists(val_path):
-                os.makedirs(val_path)
-            B = speaker_video_clip.shape[0]
-            if (batch_idx % 50) == 0:
-                for bs in range(B):
-                    render.rendering(val_path, "e{}_b{}_ind{}".format(str(epoch + 1), str(batch_idx + 1), str(bs + 1)),
-                            listener_3dmm_out[bs], speaker_video_clip[bs], listener_references[bs])
+
+            if args.render:
+                val_path = os.path.join(args.outdir, 'results_videos', 'val')
+                if not os.path.exists(val_path):
+                    os.makedirs(val_path)
+                B = speaker_video_clip.shape[0]
+                if (batch_idx % 50) == 0:
+                    for bs in range(B):
+                        render.rendering(val_path, "e{}_b{}_ind{}".format(str(epoch + 1), str(batch_idx + 1), str(bs + 1)),
+                                listener_3dmm_out[bs], speaker_video_clip[bs], listener_references[bs])
 
 
+    model.reset_window_size(args.window_size)
     return losses.avg, rec_losses.avg, kld_losses.avg
+
 
 
 def main(args):
     start_epoch = 0
     lowest_val_loss = 10000
-    train_loader = get_dataloader(args, "train", load_ref=False, load_video_l=False)
-    val_loader = get_dataloader(args, "val", load_video_l=False)
-    model = TransformerVAE(img_size = args.img_size, audio_dim = args.audio_dim,  output_3dmm_dim = args._3dmm_dim, output_emotion_dim = args.emotion_dim, feature_dim = args.feature_dim, seq_len = args.seq_len, online = args.online, window_size = args.window_size, device = args.device)
+    train_loader = get_dataloader(args, "train", load_audio=True, load_video_s=True,  load_emotion_l=True,  load_3dmm_l=True)
+    val_loader = get_dataloader(args, "val", load_audio=True, load_video_s=True,  load_emotion_l=True, load_3dmm_l=True, load_ref=True)
+    model = TransformerVAE(img_size = args.img_size, audio_dim = args.audio_dim,  output_3dmm_dim = args._3dmm_dim, output_emotion_dim = args.emotion_dim, feature_dim = args.feature_dim, seq_len = args.max_seq_len, online = args.online, window_size = args.window_size, device = args.device)
     criterion = VAELoss(args.kl_p)
 
     optimizer = optim.AdamW(model.parameters(), betas=(0.9, 0.999), lr=args.learning_rate, weight_decay=args.weight_decay)
+
     if args.resume != '':
         checkpoint_path = args.resume
         print("Resume from {}".format(checkpoint_path))
@@ -131,15 +147,15 @@ def main(args):
         render = Render()
 
     for epoch in range(start_epoch, args.epochs):
-        train_loss, rec_loss, kld_loss = train(model, train_loader, optimizer, criterion)
-        print("Epoch:  {}   train_loss: {:.5f}   train_rec_loss: {:.5f}  train_kld_loss: {:.5f} ".format(epoch+1, train_loss, rec_loss, kld_loss))
-        if epoch % 10 == 0:
+        train_loss, rec_loss, kld_loss, div_loss = train(args, model, train_loader, optimizer, criterion)
+        print("Epoch:  {}   train_loss: {:.5f}   train_rec_loss: {:.5f}  train_kld_loss: {:.5f} train_div_loss: {:.5f}".format(epoch+1, train_loss, rec_loss, kld_loss, div_loss))
+        if (epoch+1) % 10 == 0:
             val_loss, rec_loss, kld_loss = val(args, model, val_loader, criterion, render, epoch)
             print("Epoch:  {}   val_loss: {:.5f}   val_rec_loss: {:.5f}  val_kld_loss: {:.5f} ".format(epoch+1, val_loss, rec_loss, kld_loss))
             if val_loss < lowest_val_loss:
                 lowest_val_loss = val_loss
                 checkpoint = {
-                    'epoch': epoch,
+                    'epoch': epoch+1,
                     'state_dict': model.state_dict(),
                     'optimizer': optimizer.state_dict(),
                 }
@@ -148,13 +164,14 @@ def main(args):
                 torch.save(checkpoint, os.path.join(args.outdir, 'best_checkpoint.pth'))
 
         checkpoint = {
-            'epoch': epoch,
+            'epoch': epoch+1,
             'state_dict': model.state_dict(),
             'optimizer': optimizer.state_dict(),
         }
         if not os.path.exists(args.outdir):
             os.makedirs(args.outdir)
         torch.save(checkpoint, os.path.join(args.outdir, 'cur_checkpoint.pth'))
+
 
 
 # ---------------------------------------------------------------------------------
