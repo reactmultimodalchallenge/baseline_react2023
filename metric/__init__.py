768c775cf38b8eee1d49be1490ab6ac1e2aaf7bf
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2023-06-04 21:35:58 +0800

    Update __init__.py

--- metric/__init__.py
+++ metric/__init__.py
@@ -1,5 +1,5 @@
-from .FRC import compute_FRC_mp
-from .FRD import compute_FRD_mp
+from .FRC import compute_FRC_mp, compute_FRC
+from .FRD import compute_FRD_mp, compute_FRD
 from .FRDvs import compute_FRDvs
 from .FRVar import compute_FRVar
 from .S_MSE import compute_s_mse
