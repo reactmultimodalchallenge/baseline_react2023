2a2a35880de1fa6b341e53259c523a22ea15faaa
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2023-06-05 15:08:32 +0800

    Update FRC.py

--- metric/FRC.py
+++ metric/FRC.py
@@ -10,6 +10,34 @@ from functools import partial
 import multiprocessing as mp
 
 
+def corrcoef(x, y=None, rowvar=True, bias=np._NoValue, ddof=np._NoValue, *,
+             dtype=None):
+
+    if bias is not np._NoValue or ddof is not np._NoValue:
+        # 2015-03-15, 1.10
+        warnings.warn('bias and ddof have no effect and are deprecated',
+                      DeprecationWarning, stacklevel=3)
+    c = np.cov(x, y, rowvar, dtype=dtype)
+
+    try:
+        d = np.diag(c)
+    except ValueError:
+        # scalar covariance
+        # nan if incorrect value (nan, inf, 0), 1 otherwise
+        return c / c
+    stddev = np.sqrt(d.real)
+    c /= stddev[:, None]
+    c /= stddev[None, :]
+    c = np.nan_to_num(c)
+
+    # Clip real and imaginary parts to [-1, 1].  This does not guarantee
+    # abs(a[i,j]) <= 1 for complex arrays, but is the best we can do without
+    # excessive work.
+    np.clip(c.real, -1, 1, out=c.real)
+    if np.iscomplexobj(c):
+        np.clip(c.imag, -1, 1, out=c.imag)
+    return c
+
 def concordance_correlation_coefficient(y_true, y_pred,
                                         sample_weight=None,
                                         multioutput='uniform_average'):
@@ -40,9 +68,11 @@ def concordance_correlation_coefficient(y_true, y_pred,
     if len(y_true.shape) >1:
         ccc_list = []
         for i in range(y_true.shape[1]):
-            cor = np.corrcoef(y_true[:,i], y_pred[:, i])[0][1]
+            cor = corrcoef(y_true[:,i], y_pred[:, i])[0][1]
             mean_true = np.mean(y_true[:,i])
+
             mean_pred = np.mean(y_pred[:,i])
+
 
             var_true = np.var(y_true[:,i])
             var_pred = np.var(y_pred[:,i])
@@ -53,7 +83,9 @@ def concordance_correlation_coefficient(y_true, y_pred,
             numerator = 2 * cor * sd_true * sd_pred
 
             denominator = var_true + var_pred + (mean_true - mean_pred) ** 2
+
             ccc = numerator / (denominator + 1e-8)
+
             ccc_list.append(ccc)
         ccc = np.mean(ccc_list)
     else:
@@ -74,39 +106,9 @@ def concordance_correlation_coefficient(y_true, y_pred,
     return ccc
 
 
-def _func(k_neighbour_matrix, k_pred, em=None):
-    neighbour_index = np.argwhere(k_neighbour_matrix == 1).reshape(-1)
-    neighbour_index_len = len(neighbour_index)
-    max_ccc_sum = 0
-    for i in range(k_pred.shape[0]):
-        ccc_list = []
-        for n_index in range(neighbour_index_len):
-            emotion = em[neighbour_index[n_index]]
-            ccc = concordance_correlation_coefficient(emotion.numpy(), k_pred[i].numpy())
-            ccc_list.append(ccc)
-        max_ccc_sum += max(ccc_list)
-    return max_ccc_sum
 
 
 
-def compute_FRC_mp(args, pred, em, val_test='val', p=4):
-    # pred: N 10 750 25
-    # speaker: N 750 25
-
-    if val_test == 'val':
-        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_val.npy'))
-    else:
-        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_test.npy'))
-
-    FRC_list = []
-    with mp.Pool(processes=p) as pool:
-        # use map
-        _func_partial = partial(_func, em=em)
-        FRC_list += pool.starmap(_func_partial, zip(neighbour_matrix, pred))
-    return np.mean(FRC_list)
-
-
-  
 def compute_FRC(args, pred, listener_em, val_test='val'):
     pred = pred
     listener_em = listener_em
@@ -143,3 +145,33 @@ def compute_FRC(args, pred, listener_em, val_test='val'):
     return sum(all_FRC_list)
 
 
+
+def _func(k_neighbour_matrix, k_pred, em=None):
+    neighbour_index = np.argwhere(k_neighbour_matrix == 1).reshape(-1)
+    neighbour_index_len = len(neighbour_index)
+    max_ccc_sum = 0
+    for i in range(k_pred.shape[0]):
+        ccc_list = []
+        for n_index in range(neighbour_index_len):
+            emotion = em[neighbour_index[n_index]]
+            ccc = concordance_correlation_coefficient(emotion.numpy(), k_pred[i].numpy())
+            ccc_list.append(ccc)
+        max_ccc_sum += max(ccc_list)
+    return max_ccc_sum
+
+
+
+def compute_FRC_mp(args, pred, em, val_test='val', p=1):
+    # pred: N 10 750 25
+    # speaker: N 750 25
+    if val_test == 'val':
+        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_val.npy'))
+    else:
+        neighbour_matrix = np.load(os.path.join(args.dataset_path, 'neighbour_emotion_test.npy'))
+
+    FRC_list = []
+    with mp.Pool(processes=p) as pool:
+        # use map
+        _func_partial = partial(_func, em=em)
+        FRC_list += pool.starmap(_func_partial, zip(neighbour_matrix, pred))
+    return np.mean(FRC_list)
